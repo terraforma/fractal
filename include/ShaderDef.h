3e8bd41f0bd3e212958030483fd499e927d50d12
Tom Gascoigne <tom@gascoigne.me>
2013-04-15 19:21:47 +0000

    Improve lighting of landscape

--- include/ShaderDef.h
+++ include/ShaderDef.h
@@ -5,10 +5,13 @@ const char* TerrainVS = "uniform vec4 tf_LightPos; \
 varying float lightFactor; \
 varying float height; \
 void main() { \
+    vec3 normal = normalize(gl_NormalMatrix*gl_Normal).xyz; \
+    vec3 vert = (gl_ModelViewMatrix * gl_Vertex).xyz; \
+    vec3 lightPos = (gl_ModelViewMatrix * tf_LightPos).xyz; \
+    vec3 lightDir = normalize(lightPos-vert).xyz; \
+    lightFactor = dot(lightDir, normal); \
     height = gl_Vertex.z; \
     gl_TexCoord[0] = gl_MultiTexCoord0; \
-    vec4 vl = normalize(tf_LightPos - gl_Vertex); \
-    lightFactor = dot(vl, vec4(gl_NormalMatrix * gl_Normal, 0.0f)); \
     gl_FrontColor = gl_Color; \
     gl_Position = gl_ModelViewProjectionMatrix * gl_Vertex; \
 }";
